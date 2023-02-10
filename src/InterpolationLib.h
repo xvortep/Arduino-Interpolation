e27fbe56ffe163a8f7dc5ff6292e385bf011961d
xvortep <schl.petrovski@yahoo.com>
2023-02-10 18:08:43 +0100

    done

--- src/InterpolationLib.h
+++ src/InterpolationLib.h
@@ -14,78 +14,35 @@ Unless required by applicable law or agreed to in writing, software distributed 
 #include "WProgram.h"
 #endif
 
-
-template<size_t n>
-struct Range
-{
-	double list[n];
-
-	Range()
-	{
-		for (size_t m = 0; m != n; ++m)
-		{
-			list[m] = m + 1;
-		}
-	}
-
-	Range(double min, double max)
-	{
-		for (size_t m = 0; m < n; ++m)
-		{
-			list[m] = min + (max - min) / (n - 1) * m;
-		}
-	}
-
-	double& operator[](size_t  index)
-	{
-		return list[index];
-	}
-
-	double* ToArray()
-	{
-		return list;
-	}
-
-	static double* Generate(double min, double max)
-	{
-		Range<10> range(min, max);
-		return range.ToArray();
-	}
-};
-
-
-
 class Interpolation
 {
 public:
-	template <typename T>
-	static T Map(T x, T in_min, T in_max, T out_min, T out_max);
+	Interpolation(const double* x, const double* y, size_t len, double threshold = 1);
 
-	static double Step(double yValues[], int numValues, double pointX, double threshold = 1);
-	static double Step(double minX, double maxX, double yValues[], int numValues, double pointX, double threshold = 1);
-	static double Step(double xValues[], double yValues[], int numValues, double pointX, double threshold = 1);
+	//double Step(double yValues[], int numValues, double pointX, double threshold = 1);
+	//double Step(double minX, double maxX, double yValues[], int numValues, double pointX, double threshold = 1);
+	// double Step(double pointX);
+	// double Step(double pointx, int from = 0);
 
-	static double Linear(double yValues[], int numValues, double pointX, bool trim = true);
-	static double Linear(double minX, double maxX, double yValues[], int numValues, double pointX, bool trim = true);
-	static double Linear(double xValues[], double yValues[], int numValues, double pointX, bool trim = true);
-
-	static double SmoothStep(double xValues[], double yValues[], int numValues, double pointX, bool trim = true);
-	static double CatmullSpline(double xValues[], double yValues[], int numValues, double pointX, bool trim = true);
-	static double ConstrainedSpline(double xValues[], double yValues[], int numValues, double pointX, bool trim = true);
+	// double Linear(double pointX, bool trim = true);
+	double Linear(double pointX, int& cap, bool trim = true, int from = 0);
+	//double Linear(double minX, double maxX, double yValues[], int numValues, double pointX, bool trim = true);
+	//double Linear(double xValues[], double yValues[], int numValues, double pointX, bool trim = true);	
+	// double SmoothStep(double pointX, bool trim = true);
+	// double SmoothStep(double pointX, bool trim = true, int from = 0);
+	// double CatmullSpline(double pointX, bool trim = true);
+	// double CatmullSpline(double pointX, bool trim = true, int from = 0);
 
 private:
-	static double catmullSlope(double x[], double y[], int n, int i);
-	static double getFirstDerivate(double x[], double y[], int n, int i);
-	static double getLeftSecondDerivate(double x[], double y[], int n, int i);
-	static double getRightSecondDerivate(double x[], double y[], int n, int i);
+	const double* xValues;
+	const double* yValues;
+	size_t numValues;
+	double threshold;
 
+	// int getIndex(double pointX);
+	int getIndex(double pointX, int from = 0);
+
+	// double catmullSlope(int i);
 };
 
-
-// Esto esta aqui porque Arduino la lia con los Templates
-template <typename T>
-T Interpolation::Map(T x, T in_min, T in_max, T out_min, T out_max)
-{
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
 #endif
